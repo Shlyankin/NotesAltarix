a7fb952fcd34b0c99bb69ba4a1794e23860c21aa
Shlyankin Nikolay <shlyankin123@gmail.com>
2020-01-28 22:38:01 +0400

    gitignore changed

--- app/src/main/java/com/example/user/notesaltarix/MainActivity.java
+++ app/src/main/java/com/example/user/notesaltarix/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.user.notesaltarix;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
